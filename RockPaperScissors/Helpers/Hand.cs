91aeebfa3f7916f9448934b87e6f530e0076f9d3
Roman Kiss <romiki44@hotmail.com>
2021-05-11 13:39:52 +0200

    Add project files.

---
+++ RockPaperScissors/Helpers/Hand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors.Helpers
+{
+    public class Hand
+    {
+        public OptionRPS Selection { get; set; }
+        public OptionRPS WinsAgainst { get; set; }
+        public OptionRPS LosesAgainst { get; set; }
+        public string Image { get; set; }
+
+        public GamerStatus PlayAgainst(Hand opponentHand)
+        {
+            if (Selection == opponentHand.Selection)
+                return GamerStatus.Draw;
+            
+            if (LosesAgainst == opponentHand.Selection)
+                return GamerStatus.Loss;
+
+            return GamerStatus.Victory;
+        }
+    }
+}
---
+++ RockPaperScissors/Helpers/OptionRPS.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors.Helpers
+{
+    public enum OptionRPS
+    {
+        Rock,
+        Paper,
+        Scissors
+    }
+}
